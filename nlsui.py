d125aff20ca1723a12f3a609ce0890fb5990b7ab
labinxu <flowinair@gmail.com>
2018-04-12 22:07:54 +0800

    cmdbuilder

---
+++ nlsui.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+"""
+NLS web site handler
+"""
+
+import os, md5, logging, requests
+#from requests_toolbelt import MultipartEncoder
+from utils import util_requests
+from nlsauto import CMDBuilder,cmdbuild
+
+@cmdbuild
+class NLSUI(object):
+    # def __new__(cls, *args, **kwargs):
+    #     CMDBuilder.CATEGORIES={'v1' : cls}
+
+    @CMDBuilder.Args('-u','--username', default='root', help='username')
+    @CMDBuilder.Args('-p','--password', default='nls72NSN', help='password')
+    def login(self, username, password):
+        print("login :%s %s" %(username, password))
+
+if __name__=='__main__':
+    
+    CMDBuilder.run()
