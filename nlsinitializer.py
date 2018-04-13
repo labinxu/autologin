e82f28674315959d816cc905eea407dd5cf890ff
root <labinxu>
2018-04-13 15:07:25 +0800

    data file

---
+++ nlsinitializer.py
@@ -0,0 +1,118 @@
+#!/bin/env python
+
+import utils,os
+from utils import util_requests
+import optparse, md5
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class NLSDataUpload():
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.csrftoken = ''
+        self.requester = util_requests.UtilityRequests(domain='')
+        self.server = ''
+        self.url_index = 'mals60_term/go_index.do'
+        self.url_nlshome = 'mals60_term/NLSHome.do'
+
+    def _make_url(self, server, page):
+        url = 'http://{server}:8080/{page}'.format(server=server, page=page)
+        logger.debug(url)
+        return url
+
+    def login(self, username, password, server):
+        loginurl = self._make_url(server, self.url_index)
+        s = self.requester.getSoup(loginurl)
+
+        # CSRFTOKEN
+        tokentag = s.find('input', attrs={'name':'CSRFTOKEN'})
+        self.csrftoken = tokentag['value']
+        self.username = username
+        self.password = password
+        self.server = server
+
+        logger.debug(self.csrftoken)
+        m = md5.new()
+        m.update(password)
+
+        playdata = {'CSRFTOKEN': tokentag['value'],
+                    'txt_popupwindowvalue': '0',
+                    'languageSelect': 'English',
+                    'username': self.username,
+                    'password': m.hexdigest(),
+                    'password_i': '*'*len(password)
+        }
+
+        logger.debug(str(playdata))
+        urlhome = self._make_url(server, self.url_nlshome)
+        resp = self.requester.post(urlhome, data=playdata)
+        status =  True if resp.text.find('Sorry') == -1 else False
+        if not status:
+            with open('./loginerror.html','w') as f:
+                f.write(resp.text)
+
+        return status
+
+
+    def _addreferer(self):
+        referer = 'mals60_term/aspsa/asps101?CSRFTOKEN=%s'% self.csrftoken
+        referer = self._make_url(self.server, referer)
+        header  = {'Referer':referer}
+        self.requester.addHeaders(header)
+
+    def addSP(self, data):
+        self._addreferer()
+        addsp = 'mals60_term/aspsa/asps102?CSRFTOKEN=%s' % self.csrftoken
+        url = self._make_url(self.server, addsp)
+        header = {'Content-Type': 'application/x-www-form-urlencoded'}
+        self.requester.addHeaders(header)
+#        import pdb;pdb.set_trace()
+        if data:
+            data['CSRFTOKEN'] = self.csrftoken
+            resp = self.requester.post(url, data=data)
+            return resp.text.find('Add SP Basic Info successfully') != -1
+
+        return False
+    
+    def addAppID(self, data):
+        self._addreferer()
+        addappid = 'mals60_term/aspsa/aspsd02?CSRFTOKEN=%s'%self.csrftoken
+        url = self._make_url(self.server, addappid)
+        header = {'Content-Type': 'application/x-www-form-urlencoded'}
+        self.requester.addHeaders(header)
+        if data:
+            data['CSRFTOKEN'] = self.csrftoke
+            resp = self.requester.post(url, data=data)
+            return resp.text.find('Add SET Application ID successfully') != -1
+
+        return False
+
+
+    def uploadLTEBSData(self, f):
+        from requests_toolbelt import MultipartEncoder
+        logger.info('upload %s' %f)
+        ct = 'application/octet-stream'
+        page = 'mals60_term/absaa/absa102?CSRFTOKEN=%s' % self.csrftoken
+        url_LTEdata = self._make_url(self.server, page)
+
+        fields=(
+            ('selectID','6'),
+            ('file1', (os.path.basename(f), open(f, 'rb'), 'application/vnd.ms-excel')),
+            ('file2',('',None, ct)),
+            ('file3',('',None, ct)),
+            ('isOper','no'),
+            ('CSRFTOKEN',self.csrftoken)
+        )
+
+        m = MultipartEncoder(fields)
+        self.requester.addHeaders({'Upgrade-Insecure-Requests':"1"})
+        self.requester.addHeaders({'Content-Type':m.content_type})
+        resp = self.requester.post(url_LTEdata, data=m)
+        status = True if resp.text.find('Upload File successfully') != -1 else False
+        if not status:
+            with open('./uploadlete.html', 'w') as f:
+                f.write(resp.text)
+        return status
--- nlsui.py
+++ nlsui.py
@@ -7,16 +7,80 @@ NLS web site handler
 import os, md5, logging, requests
 #from requests_toolbelt import MultipartEncoder
 from utils import util_requests
-from nlsauto import CMDBuilder,cmdbuild
+from nlsinitializer import NLSDataUpload
+from commandline import CMDBuilder, cmdbuild
+import logging, json
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 @cmdbuild('nls')
 class NLSUI(object):
-    # def __new__(cls, *args, **kwargs):
-    #     CMDBuilder.CATEGORIES={'v1' : cls}
+    def __init__(self):
+        self.nlsinitializer = NLSDataUpload()
+
     @CMDBuilder.Args('-u','--username', default='root', help='username')
     @CMDBuilder.Args('-p','--password', default='nls72NSN', help='password')
-    def login(self, username, password):
-        print("login: %s %s" %(username, password))
+    @CMDBuilder.Args('-s', '--server', default='10.129.113.132', help='server')
+    def login(self, username, password, server):
+        return self.nlsinitializer.login(username, password, server)
+    
+    @CMDBuilder.Args('ltedata', help='lte bs ok data file')
+    def initltebsdata(self, ltedata):
+        return self.nlsinitializer.uploadLTEBSData(ltedata)
 
+    @CMDBuilder.Args('spdata', help='sp data file')
+    def initspdata(self, spdata):
+        return self.nlsinitializer.addSP(spdata)
+
+    @CMDBuilder.Args('-u', '--username', default='root', help='username')
+    @CMDBuilder.Args('-p', '--password', default='nls72NSN', help='password')
+    @CMDBuilder.Args('-s', '--server', default='10.129.113.132', help=' server ip')
+    @CMDBuilder.Args('-l', '--ltedata', default='', help='the lte base data file type is csv')
+    @CMDBuilder.Args('-S', '--spdata', default='', help='SP data file')
+    @CMDBuilder.Args('-a', '--appid', default='', help='App ID data file')
+    def init(self, username, password, server, ltedata, spdata, appid):
+        if self.login(username, password, server):
+            logger.info('Login successfully')
+        else:
+            logger.error('Login failed')
+        #
+        if ltedata:
+            if self.initltebsdata(ltedata):
+                logger.info('LTE BS Data Upload successfully')
+            else:
+                logger.error('LTE BS Data Upload Failed!')
+
+        payload = {}
+        if spdata and os.path.exists(spdata):
+            import pdb; pdb.set_trace()
+            with open(spdata) as f:
+                payload = json.load(f)
+            spdata= {                              
+                'save':'save',              
+                'userid':'laxu',            
+                'password':'111',           
+                'privatelevel':'1',         
+                "vaildtime":"2020-12-31",   
+                "activeflag":"1",           
+                "version":"10",             
+                "delineation":"description",
+                "xydisplay":"0",            
+                "ipauthentication":"1",     
+                "coarselocationmark":"1",   
+                "CSRFTOKEN":''
+            }                                   
+
+            if not payload:
+                logger.error('The spdata read failed')
+            else:
+                if self.initspdata(payload):
+                    logger.info('Add SP Data successfully')
+                else:
+                    logger.error('Add SP Data Failed!')
+        payload = {}
+        
 if __name__=='__main__':
     CMDBuilder.run()
